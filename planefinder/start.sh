a7c180ee9dd2d6ecfe201d101fdc22c90240a617
ShoGinn <2660603+ShoGinn@users.noreply.github.com>
2018-01-06 15:41:51 -0800

    stuff

--- planefinder/start.sh
+++ planefinder/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/pfclient --address=$DUMP1090_HOST --port=$DUMP1090_PORT --sharecode=$PF_SHARECODE --lat=$LAT --long=$LONG --data_format=1
+/usr/bin/pfclient --address=$DUMP1090_HOST --port=$DUMP1090_PORT --sharecode=$PF_SHARECODE --lat=$LAT --lon=$LONG --data_format=1
