07d0bbd1dceff738033005151b05cfcb8ea78033
ShoGinn <2660603+ShoGinn@users.noreply.github.com>
2018-01-06 11:42:39 -0800

    Update startdumping.sh

--- dump1090/startdumping.sh
+++ dump1090/startdumping.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./dump1090 --net-http-port $DUMP_HTTP_PORT --net --net-sbs-port $DUMP_SBS_PORT --interactive --lat $DUMP_LAT --lon $DUMP_LON
+/usr/bin/dump1090-mutability --net-http-port $DUMP_HTTP_PORT --net --net-sbs-port $DUMP_SBS_PORT --interactive --lat $DUMP_LAT --lon $DUMP_LON
