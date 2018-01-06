f86ce2a51618042a0317e5c4d4a770bf78bb20b9
ShoGinn <2660603+ShoGinn@users.noreply.github.com>
2018-01-06 15:50:44 -0800

    format

--- dump1090/start.sh
+++ dump1090/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/dump1090-mutability --net-http-port $DUMP_HTTP_PORT --net --net-sbs-port $DUMP_SBS_PORT --interactive --lat $DUMP_LAT --lon $DUMP_LON
+/usr/bin/dump1090-mutability --quiet --net --net-sbs-port $DUMP_SBS_PORT --lat $DUMP_LAT --lon $DUMP_LON
